f597e51edebe5de26c6db05bbae2f26d3447e136
Evan Kennedy <evank@fb.com>
2013-06-17 14:07:59 -0500

    organization list

--- src/main/java/com/evan/webapp/action/OrganizationAction.java
+++ src/main/java/com/evan/webapp/action/OrganizationAction.java
@@ -16,13 +16,32 @@ import java.util.List;
 public class OrganizationAction extends BaseAction {
     private GenericManager<Organization, Long> organizationManager;
     private List<Organization> organizations;
+    private String id;
 
-    public String execute() {
-        return INPUT;
+    private static final String ACTION_LIST = "list";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     @Autowired
     public void setOrganizationManager(GenericManager<Organization, Long> organizationManager) {
         this.organizationManager = organizationManager;
+    }
+
+    public void prepare() {
+        System.out.println("this is a test");
+    }
+
+    public String execute() {
+        return SUCCESS;
+    }
+
+    public String list() {
+        return ACTION_LIST;
     }
 }
